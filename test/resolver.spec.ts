5895674f1d9be007614fc142ab353581d5572390
Dmitriy Ryajov <dryajov@gmail.com>
2019-04-23 01:53:48 -0600

    feat: initial commit of decorators for opium

---
+++ test/resolver.spec.ts
@@ -0,0 +1,39 @@
+/* eslint-env mocha */
+
+'use strict'
+
+import * as chai from 'chai'
+import dirtyChai = require('dirty-chai')
+const expect = chai.expect
+chai.use(dirtyChai)
+
+import { inject, app } from '../src'
+
+describe('decorators', () => {
+  it('should register class', () => {
+    @inject
+    class MyClass {
+      public greet: string
+
+      constructor() {
+         this.greet = 'hello world!'
+      }
+    }
+
+    @app
+    class MyApp {
+      constructor(param1: MyClass) {
+        expect(param1.greet).to.eq('hello world!')
+      }
+    }
+  })
+
+  it('should inject function', () => {
+    class MyClass {
+      @inject()
+      func (param1: string) {
+        console.log(`this is my class`, param1)
+      }
+    }
+  })
+})
